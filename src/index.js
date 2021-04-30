579b8f6eaef6cc7db339d3e6c82993a3ab291f20
pkiop <pkiop@naver.com>
2021-04-30 17:57:54 +0900

    javascript

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
+// const openModalBtn = document.querySelector('.open-modal-btn');
+// const openModalContent = document.querySelector('.modal-contents');
+// const buttons = document.querySelector('.buttons');
+// console.log("buttons : ", buttons);
+
+
 const modal = document.querySelector('.modal');
-const openModalBtn = document.querySelector('.open-modal-btn');
-const openModalContent = document.querySelector('.modal-contents');
-const buttons = document.querySelector('.buttons');
-console.log("buttons : ", buttons);
-
-
 modal.addEventListener('click', () => {
-  console.log("modal clicked");
-  modal.classList.remove('active');
+  modal.classList.remove('abc');
 });
 
-openModalContent.addEventListener('click', (e) => {
-  e.stopPropagation();
-})
+// openModalContent.addEventListener('click', (e) => {
+//   e.stopPropagation();
+// })
 
-buttons.addEventListener('click', (e) => {
-  console.log("e : ", e.path[1]);
-  console.log("e : ", e.target.className);
-})
+// buttons.addEventListener('click', (e) => {
+//   console.log("e : ", e.path[1]);
+//   console.log("e : ", e.target.className);
+// })
 
-console.log('modal : ', modal);
+// console.log('modal : ', modal);
 
