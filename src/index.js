606a47c1df7590bd421988c2c2ee8baa6e8186a4
pkiop <pkiop@naver.com>
2021-04-30 22:32:27 +0900

    Feat: Add stopPropagation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-// const openModalBtn = document.querySelector('.open-modal-btn');
-// const openModalContent = document.querySelector('.modal-contents');
-// const buttons = document.querySelector('.buttons');
-// console.log("buttons : ", buttons);
-
+const openModalContent = document.querySelector('.modal-contents');
 
 const modal = document.querySelector('.modal');
 modal.addEventListener('click', () => {
   modal.classList.remove('abc');
 });
 
-// openModalContent.addEventListener('click', (e) => {
-//   e.stopPropagation();
-// })
+openModalContent.addEventListener('click', (e) => {
+  e.stopPropagation();
+})
 
-// buttons.addEventListener('click', (e) => {
-//   console.log("e : ", e.path[1]);
-//   console.log("e : ", e.target.className);
-// })
-
-// console.log('modal : ', modal);
 
