644bef4b2a0fb7d8ce1d7ae18d52b6e64cf74f7a
pkiop <pkiop@naver.com>
2021-05-01 13:17:57 +0900

    Refector: Fix index.css to style.css

---
+++ server/public/search/index.js
@@ -0,0 +1,61 @@
+console.log("search render");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const openModalContent = document.querySelector('.modal-contents');
+// const openBtn = document.querySelector('.open');
+// const email = document.querySelector('.email');
+
+// const modal = document.querySelector('.modal');
+// openBtn.addEventListener('click', () => {
+//   modal.classList.add('abc');
+// });
+
+// modal.addEventListener('click', () => {
+//   console.log('modal clicked');
+//   modal.classList.remove('abc');
+// });
+
+// openModalContent.addEventListener('click', (event) => {
+//   console.log('openModalContent clicked');
+// })
+
+// email.addEventListener('click', function(event) {
+//   console.log(event);
+//   event.stopPropagation();
+//   console.log('email clicked');
+// })
+
+
+
+
