2e0e18288cedc95b48607987dcf9835d90aa1517
pkiop <pkiop@naver.com>
2021-05-01 12:33:46 +0900

    Feat: Add index.html router

---
+++ server/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
